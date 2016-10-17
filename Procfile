aabf84db43143806c88d067ec7b4fa165a1f132b
solopav <solopav.job@ya.ru>
2016-10-17 09:14:33 +0300

    settings for heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node server.js -p $PORT
+web: node server.js
--- server.js
+++ server.js
@@ -1,23 +1,14 @@
-var webpack = require('webpack')
-var webpackDevMiddleware = require('webpack-dev-middleware')
-var webpackHotMiddleware = require('webpack-hot-middleware')
-var config = require('./webpack.config')
-
 var express = require('express')
-var app = new (require('express'))()
-var port = 8080
+var app = express()
 
-var compiler = webpack(config)
-
-app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
-app.use(webpackHotMiddleware(compiler))
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/'));
 
-app.listen(port, function(error) {
-  if (error) {
-    console.error(error)
-  } else {
-    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
-  }
-})
+app.get('/', function(request, response) {
+  response.render('pages/index')
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
